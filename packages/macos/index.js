0d8c9d89accb0907b47e80103513b5773c42e6dd
Morgan Spencer <morgan@spencercreative.co>
2021-12-11 17:30:34 -0500

    Update package details

--- packages/macos/index.js
+++ packages/macos/index.js
@@ -2,4 +2,7 @@ import { AppRegistry } from "react-native";
 import { App } from "@deskjockeyfitness/app";
 import { name as appName } from "./app.json";
 
+/* URL polyfill. Required for Supabase queries to work in React Native. */
+import 'react-native-url-polyfill/auto'
+
 AppRegistry.registerComponent(appName, () => App);
